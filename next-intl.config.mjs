becc04411a111bbe4e369d87518c48893c45a616
Elaman Zarlykov <z.elaman699@gmail.com>
2026-03-15 22:29:49 +0600

    fix: delete next-intl

--- next-intl.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-intl').IntlConfig} */
-const nextIntlConfig = {
-  locales: ["ru", "en", "ky", "zh"],
-  defaultLocale: "ru",
-};
-
-export default nextIntlConfig;
-
--- next-intl.config.ts
+++
@@ -1,8 +0,0 @@
-import type { IntlConfig } from "next-intl";
-
-const nextIntlConfig: IntlConfig = {
-  locales: ["ru", "en", "ky", "zh"],
-  defaultLocale: "ru",
-};
-
-export default nextIntlConfig;
