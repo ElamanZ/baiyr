86c56ffa6b5499e759d43109bccf7e4476044a1f
ElamanZ <ElamanZ@github.com>
2026-03-18 16:21:52 +0600

    feat: footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,59 +1,51 @@
 "use client";
 
+import { useTranslations } from "next-intl";
+import styles from "./Footer.module.css";
 import InstagramIcon from "../icons/InstagramIcon";
 import PhoneIcon from "../icons/PhoneIcon";
-import styles from "./Footer.module.css";
+import LogoIcon from "../icons/LogoIcon";
+import { useBreakpoint } from "@/hooks/use-breakpoint";
 
 export default function Footer() {
+  const t = useTranslations("Footer");
+
+  const isMd = !useBreakpoint("md");
+
   return (
     <footer className={styles.footer}>
-      <div className={styles.inner}>
-        <article className={styles.card}>
-          <div className={styles.cardTop}>
-            <span className={styles.icon}>
-              <InstagramIcon size={40} />
-            </span>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <LogoIcon size={170} />
+        </div>
 
-            <div className={styles.textBlock}>
-              <span className={styles.label}>Instagram</span>
-              <strong className={styles.value}>baiyr_honey</strong>
+        <div className={styles.cardBlock}>
+          <article className={styles.card}>
+            <div className={styles.cardTop}>
+              <InstagramIcon size={isMd ? 30 : 50} />
+
+              <div className={styles.textBlock}>
+                <p>{t("instagramLabel")}</p>
+                <h4>{t("instagramValue")}</h4>
+              </div>
             </div>
-          </div>
 
-          <button
-            className={styles.button}
-            onClick={() =>
-              window.open("https://www.instagram.com/baiyr_honey", "_blank")
-            }
-          >
-            Открыть Страницу
-          </button>
-        </article>
+            <button className={styles.button}>{t("instagramButton")}</button>
+          </article>
 
-        <article className={styles.card}>
-          <div className={styles.cardTop}>
-            <span className={styles.icon}>
-              <PhoneIcon size={40} />
-            </span>
+          <article className={styles.card}>
+            <div className={styles.cardTop}>
+              <PhoneIcon size={isMd ? 30 : 50} />
 
-            <div className={styles.textBlock}>
-              <span className={styles.label}>Номер Телефона</span>
-              <strong className={styles.value}>+996 (708) 45-45-45</strong>
+              <div className={styles.textBlock}>
+                <p>{t("phoneLabel")}</p>
+                <h4>{t("phoneValue")}</h4>
+              </div>
             </div>
-          </div>
 
-          <button
-            className={styles.button}
-            onClick={() =>
-              window.open(
-                "https://wa.me/996502707800?text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5,%20%D1%85%D0%BE%D1%87%D1%83%20%D0%B7%D0%B0%D0%BA%D0%B0%D0%B7%D0%B0%D1%82%D1%8C%20%D0%BC%D0%B5%D0%B4!%20",
-                "_blank",
-              )
-            }
-          >
-            Написать
-          </button>
-        </article>
+            <button className={styles.button}>{t("phoneButton")}</button>
+          </article>
+        </div>
       </div>
     </footer>
   );
